10c0802bb0492b9d92125363e4ffac4c2d58ea40
BrunoFontouraIF <bruno.fontoura.ifpr@gmail.com>
2025-09-03 16:04:13 -0300

    func

---
+++ js/index.js
@@ -0,0 +1,19 @@
+/* Tentativas, número sorteado, número adivinhado */
+var t, n, a
+  
+    a = parseInt(prompt("Adivinhe o número: "))
+
+    /* Lógica */
+    function sortear() {
+
+        /* Limite máximo e minimo */
+        const min = 0;
+        const max = 10;
+
+        const sort = Math.floor((Math.random() * max) + min);
+
+    }
+
+
+
+
