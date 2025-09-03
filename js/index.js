caccef128db2b8de164a2e5bcb89b5a25c3f7f4a
BrunoFontouraIF <bruno.fontoura.ifpr@gmail.com>
2025-09-03 16:16:16 -0300

    Randomização

--- js/index.js
+++ js/index.js
@@ -1,19 +1,3 @@
-/* Tentativas, número sorteado, número adivinhado */
-var t, n, a
-  
-    a = parseInt(prompt("Adivinhe o número: "))
-
-    /* Lógica */
-    function sortear() {
-
-        /* Limite máximo e minimo */
-        const min = 0;
-        const max = 10;
-
-        const sort = Math.floor((Math.random() * max) + min);
-
-    }
-
-
-
-
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
