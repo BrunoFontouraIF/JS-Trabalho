0f5b3b3b90605fb51530f3d1a33a2218edf0818a
BrunoFontouraIF <bruno.fontoura.ifpr@gmail.com>
2025-09-10 15:39:16 -0300

    Jogo realizado.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+ const n_adivinhar = getRndInteger(1, 100);
+
+        function getRndInteger(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+
+        function verificar() {
+            const numero = document.getElementById('numero').value;
+
+            // Verificação final
+            if (parseInt(numero) === n_adivinhar) {
+                alert("Você acertou!");
+                location.reload();
+            } else {
+                alert(`Que pena. O número na verdade era ${n_adivinhar}.`);
+                location.reload();
+            }
+        }
